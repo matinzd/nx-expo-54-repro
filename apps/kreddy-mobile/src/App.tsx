a164aadef2065f4e935650e3471913d115381fb0
matin-zadeh-dolatabad <matin@lendo.se>
2025-10-22 22:30:24 +0200

    try to fix rn exports

--- apps/kreddy-mobile/src/App.tsx
+++ apps/kreddy-mobile/src/App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-// import { sharedLib } from '@test-monorepo/shared-lib';
+import { sharedLib } from '@test-monorepo/shared-lib';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      {/* <Text>Shared Lib Name: {sharedLib()}</Text> */}
+      <Text>Shared Lib Name: {sharedLib()}</Text>
       <Text>{'Welcome to mobile app!'}</Text>
     </View>
   );
